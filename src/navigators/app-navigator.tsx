1f6f91e8c0fe8b1b1a26a50393279a38eed894e3
David Potgieter <david@weswingcats.com>
2023-08-12 13:10:31 +0700

    plugged in new layout

--- src/navigators/app-navigator.tsx
+++ src/navigators/app-navigator.tsx
@@ -1,13 +1,10 @@
+import { HomeScreen } from '@/screens/home-screen'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import RNBootSplash from 'react-native-bootsplash'
 import 'react-native-reanimated'
 
 const { Navigator, Screen } = createNativeStackNavigator()
-
-export function HomeScreen(): JSX.Element {
-  return <></>
-}
 
 export default function AppNavigator(): JSX.Element {
   return (
