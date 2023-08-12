0923b61782780c62821d8fff2317ab8f397e3e1f
David Potgieter <david@weswingcats.com>
2023-08-12 10:55:34 +0700

    added and configured react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import AppNavigator from '@/navigators/app-navigator'
+import { ReactQueryProvider } from '@/providers/react-query/query-provider'
 
 export default function App(): JSX.Element {
-  return <AppNavigator />
+  return (
+    <ReactQueryProvider>
+      <AppNavigator />
+    </ReactQueryProvider>
+  )
 }
