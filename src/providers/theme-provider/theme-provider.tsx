9b6f45d09b23e95fd553e92d718727a87e90a0a6
David Potgieter <david@weswingcats.com>
2023-08-14 09:43:14 +0700

    added nativebase back in with config

--- src/providers/theme-provider/theme-provider.tsx
+++ src/providers/theme-provider/theme-provider.tsx
@@ -1,27 +1,26 @@
-import { default as theme } from '@/providers/theme-provider/hacker-news-theme.json'
-import * as eva from '@eva-design/eva'
-import { ApplicationProvider, IconRegistry } from '@ui-kitten/components'
-import { EvaIconsPack } from '@ui-kitten/eva-icons'
+import { hackerNewsColors, hackerNewsTheme } from '@/providers/theme-provider/hacker-news-theme'
+import { NativeBaseProvider, extendTheme } from 'native-base'
 import { StyleSheet, View } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
   const insets = useSafeAreaInsets()
-  const styles = StyleSheet.create({
+  const theme = extendTheme(hackerNewsTheme)
+
+  const base = StyleSheet.create({
     container: {
       flex: 1,
       paddingTop: insets.top,
       paddingBottom: insets.bottom,
       paddingLeft: insets.left,
       paddingRight: insets.right,
+      backgroundColor: hackerNewsColors['color-gray-500'],
     },
   })
+
   return (
-    <>
-      <IconRegistry icons={EvaIconsPack} />
-      <ApplicationProvider {...eva} theme={{ ...eva.dark, ...theme }}>
-        <View style={styles.container}>{children}</View>
-      </ApplicationProvider>
-    </>
+    <NativeBaseProvider theme={theme}>
+      <View style={base.container}>{children}</View>
+    </NativeBaseProvider>
   )
 }
