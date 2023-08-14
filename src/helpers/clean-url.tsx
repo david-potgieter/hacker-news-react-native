074bbc7f9760925f4c8f4d68f2e4965ace6eaa54
David Potgieter <david@weswingcats.com>
2023-08-14 13:34:09 +0700

    removed comment

--- src/helpers/clean-url.tsx
+++ src/helpers/clean-url.tsx
@@ -1,12 +1,3 @@
-/**
- * Story title
- * Story URL
- * Story timestamp
- * Story score
- * Author id
- * Author karma score
- */
-
 export function cleanUrl(url: string) {
   return url.replace(/^https?:\/\/|\/.*$/gm, '')
 }
