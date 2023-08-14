5ea43296eca910f8d8a6b0528aba1f24aabf3ffb
David Potgieter <david@weswingcats.com>
2023-08-14 13:16:15 +0700

    fixed layout for darkmode

--- src/screens/home-screen.tsx
+++ src/screens/home-screen.tsx
@@ -1,9 +1,11 @@
 import { AppLayout } from '@/components/layout/app-layout'
 import { StoriesList } from '@/components/list/stories-list'
+import { StatusBar } from 'react-native'
 
 export function HomeScreen(): JSX.Element {
   return (
     <AppLayout>
+      <StatusBar barStyle="light-content" />
       <StoriesList />
     </AppLayout>
   )
